812e8a9286035e31e12baf35fb057afb72511942
syafiudin <msyaviudin@gmail.com>
2020-07-24 00:27:09 +0700

    finish

--- script.js
+++ script.js
@@ -65,6 +65,9 @@ const calculate = ()=>{
         case `/`:
             result = prevNumber / currentNumber
             break
+        case `%`:
+            result = prevNumber % currentNumber
+            break
         default:
             break
     }
