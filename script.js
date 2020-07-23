1510f4927e5e3ec07e64843e59cd53f1ebf00029
syafiudin <msyaviudin@gmail.com>
2020-07-23 13:36:30 +0700

    langkah 4

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let prevNumber = ``
+let calculationOperator = ``
+let currentNumber = `0`
+
+const inputNumber = (number) => {
+    if(currentNumber === `0`){
+        currentNumber = number
+    }else{
+        currentNumber += number
+    }
+}
+const operators = document.querySelectorAll(`.operator`)
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+const inputOperator = (operator) => {
+    prevNumber = currentNumber
+    calculationOperator = operator
+    currentNumber = ``
+}
+
 const calculatorScreen = document.querySelector(`.calculator-screen`)
 
 const updateScreen = (number) => {
@@ -7,7 +31,7 @@ const updateScreen = (number) => {
 const numbers = document.querySelectorAll(".number")
 numbers.forEach((number) => {
     number.addEventListener("click", (event) =>{
-        updateScreen(event.target.value)
+        updateScreen(currentNumber)
     })
     
 })
